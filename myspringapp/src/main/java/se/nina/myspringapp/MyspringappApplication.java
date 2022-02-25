4e3dbff5412daf65a519605e81fb2f2addd78f8d
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2022-02-25 11:00:25 +0100

    Initial commit

---
+++ myspringapp/src/main/java/se/nina/myspringapp/MyspringappApplication.java
@@ -0,0 +1,14 @@
+package se.nina.myspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringappApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MyspringappApplication.class, args);
+	}
+
+}
